c8315adfdf3a7bdb160f0f37282a28182c927f68
Kamar <abukamar1805@gmail.com>
2025-06-24 23:37:24 +0100

    turned it to a pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
+// Register service worker
+import { registerSW } from 'virtual:pwa-register';
+registerSW();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
